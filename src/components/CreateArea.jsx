6bd082df865c20ecda91d23d53ff7ecc53be523b
Matthias Mau <matthias.mau@outlook.com>
2020-09-22 13:19:36 +0300

    implemented add and delete note functionality

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+    
+    const [inputNote, setInputNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+    
+        setInputNote(prevNote => {
+          return {
+            ...prevNote,
+            [name]: value
+          };
+        });
+    }
+
+    function submitNote(event) {
+        props.onAdd(inputNote);
+        setInputNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault();
+    }
+  
+    return (
+        <div>
+        <form>
+            <input 
+                onChange={handleChange}
+                name="title"
+                value={inputNote.title} 
+                placeholder="Title"
+            />
+            <textarea 
+                onChange={handleChange}
+                name="content"
+                value={inputNote.content} 
+                placeholder="Take a note..." 
+                rows="3" 
+            />
+            <button onClick={submitNote}>Add</button>
+        </form>
+        </div>
+  );
+}
+
+export default CreateArea;
